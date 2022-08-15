92b3f8f5a91aa88de120697b78f8a7b71876a7c1
pitzzahh <araopj2191@gmail.com>
2022-08-15 15:02:07 +0800

    added joke and sum slash commands

--- src/main/java/io/github/pitzzahh/commands/CommandListener.java
+++ src/main/java/io/github/pitzzahh/commands/CommandListener.java
@@ -1,10 +1,9 @@
 package io.github.pitzzahh.commands;
 
-import io.github.pitzzahh.Util;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-
+import io.github.pitzzahh.Util;
 import java.util.Random;
 
 public class CommandListener extends ListenerAdapter {
@@ -14,7 +13,26 @@ public class CommandListener extends ListenerAdapter {
         var command = event.getName();
         switch (command) {
             case "joke" -> {
-                var pick = RANDOM.nextInt(Util.JOKES.length + 1);
+                var pick = RANDOM.nextInt(Util.JOKES.length);
+                var joke = Util.JOKES[pick].split("[?]");
+                event.reply(joke[0] + "?").queue();
+                try {
+                    Thread.sleep(Util.getDelay(joke[0]));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                event.getChannel().sendMessage(joke[1]).queue();
+                event.getChannel().sendMessage(":rofl:").queue();
+            }
+            case "sum" -> {
+                var firstNumber = event.getOption("firstnumber");
+                var secondNumber = event.getOption("secondnumber");
+
+                if(firstNumber == null || secondNumber == null) return;
+
+                var sum = firstNumber.getAsInt() + secondNumber.getAsInt();
+
+                event.reply("The sum is: " + sum).queue();
             }
         }
     }
