2f46a1665f7869fbce187df4cbd44c357d974d9b
pitzzahh <araopj2191@gmail.com>
2022-08-15 14:59:30 +0800

    refactored slash commands

--- src/main/java/io/github/pitzzahh/App.java
+++ src/main/java/io/github/pitzzahh/App.java
@@ -1,33 +1,37 @@
 package io.github.pitzzahh;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import org.jetbrains.annotations.NotNull;
 import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import io.github.pitzzahh.events.MessageListener;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import io.github.pitzzahh.commands.CommandListener;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 public class App extends ListenerAdapter {
 
-    public static void main(@NotNull String[] args) throws LoginException {
-        JDA jda = JDABuilder
+    public static void main(String[] args) throws LoginException {
+        if (args.length == 0) throw new IllegalStateException("TOKEN NOT PROVIDED");
+        var jda = JDABuilder
                 .createDefault(args[0])
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .setActivity(Activity.listening("your messages \uD83D\uDCE9"))
-                .addEventListeners(new MessageListener()).build();
-        var guild = jda.getGuildById("1008377788799123496");
-        if (guild == null) throw new IllegalStateException("Server ID is not valid");
-        guild.upsertCommand("joke", "Tells a random joke").queue();
-        guild.upsertCommand("sum", "Sums two given numbers")
+                .addEventListeners(new CommandListener())
+                .addEventListeners(new MessageListener())
+                .build();
+
+        var commands = jda.updateCommands();
+        commands.addCommands(Commands.slash("joke", "Tells a random joker"));
+        commands.addCommands(Commands.slash("sum", "Add two numbers")
                 .addOptions(
-                        new OptionData(OptionType.INTEGER, "first operand", "the first number", true)
+                        new OptionData(OptionType.INTEGER, "firstnumber", "the first number", true)
                                 .setRequiredRange(1, Integer.MAX_VALUE),
-                        new OptionData(OptionType.INTEGER, "second operand", "the second number", true)
+                        new OptionData(OptionType.INTEGER, "secondnumber", "the second number", true)
                                 .setRequiredRange(1, Integer.MAX_VALUE)
-                ).queue();
+                ));
+        commands.queue();
     }
 }
