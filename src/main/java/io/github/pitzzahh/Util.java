dd50665464565ace25eba41707653b61128788f6
pitzzahh <araopj2191@gmail.com>
2022-08-15 15:04:55 +0800

    added getDelay() function

--- src/main/java/io/github/pitzzahh/Util.java
+++ src/main/java/io/github/pitzzahh/Util.java
@@ -23,13 +23,44 @@ public class Util {
             "What do you call a fake noodle? An impasta.",
             "What does it make you if you see a robbery at an Apple Store? An iwitness.",
             "What is an astronaut’s favorite key on a keyboard? The space bar.",
-            "Can February March? No but April May",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "Can February March? No but April May"
     };
+
+    /**
+     * Gets a delay based on the length of the joke.
+     * @param question the quesiton joke.
+     * @return an {@code int} the lenght of the delay.
+     */
+    public static int getDelay(final String question) {
+        switch (question.length()) {
+            case 10,11,12,13,14 -> {
+                return 500;
+            }
+            case 15,16,17,18,19 -> {
+                return 1000;
+            }
+            case 20,21,22,23,24 -> {
+                return 1500;
+            }
+            case 25,26,27,28,29 -> {
+                return 2000;
+            }
+            case 30,31,32,33,34 -> {
+                return 2500;
+            }
+            case 35,36,37,38,39 -> {
+                return 3000;
+            }
+            case 40,41,42,43,44 -> {
+                return 3500;
+            }
+            case 45,46,47,48,49 -> {
+                return 4000;
+            }
+            case 50,51,52,53,54,55,56,57,58,59 -> {
+                return 4500;
+            }
+        }
+        return 5000;
+    }
 }
