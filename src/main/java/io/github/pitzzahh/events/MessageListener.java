b7f9555f13adb1d49cebaa1a3ae4573515639502
pitzzahh <araopj2191@gmail.com>
2022-08-17 00:05:24 +0800

    refactored the bad message and warning, delete both messages at given time.

--- src/main/java/io/github/pitzzahh/events/MessageListener.java
+++ src/main/java/io/github/pitzzahh/events/MessageListener.java
@@ -2,6 +2,7 @@ package io.github.pitzzahh.events;
 
 import java.awt.*;
 import io.github.pitzzahh.Util;
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 import net.dv8tion.jda.api.EmbedBuilder;
 import org.jetbrains.annotations.NotNull;
@@ -27,18 +28,29 @@ public class MessageListener extends ListenerAdapter {
                     event.getMessage().reply(String.format("Hello %s!", user)).queue();
                 }
                 default -> {
+                    var m = event.getMessage().getContentRaw();
                     var isBad = Util.BAD_WORDS
                             .stream()
-                            .anyMatch(e -> e.equalsIgnoreCase(messageSent));
+                            .anyMatch(e -> e.equalsIgnoreCase(m));
+                    System.out.println("m = " + m);
+                    System.out.println("isBad = " + isBad);
                     if (isBad) {
                         var message = new EmbedBuilder();
                         message.setColor(Color.RED)
-                               .setTitle("PEASANT ALERT!")
-                               .setDescription(String.format("This \'%s\' message seems sus..", event.getMessage().getContentRaw()))
-                                .setFooter("your suss message will be deleted after 20 seconds");
+                                .setTitle("PEASANT ALERT!")
+                                .setDescription(String.format("This message seems sus..", event.getMessage().getContentRaw()))
+                                .setDescription("your suss message will be deleted after 5 seconds")
+                                .setTimestamp(LocalDateTime.now())
+                                .setFooter("this message will be deleted after 1 minute");
+
                         event.getMessage()
                                 .replyEmbeds(message.build())
-                                .queue(e -> e.delete().queueAfter(20, TimeUnit.SECONDS));
+                                .queue(e -> {
+                                    event.getMessage().delete()
+                                            .reason("BAD WORDS ARE NOT ALLOWED")
+                                            .queueAfter(5, TimeUnit.SECONDS);
+                                    e.delete().queueAfter(1, TimeUnit.MINUTES);
+                                });
                     }
                 }
             }
