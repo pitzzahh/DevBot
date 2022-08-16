4915f49f60268fd8e8db30b0101978eca59ac777
pitzzahh <araopj2191@gmail.com>
2022-08-16 20:55:29 +0800

    removed onMessageReactionRemove() method

--- src/main/java/io/github/pitzzahh/events/MessageListener.java
+++ src/main/java/io/github/pitzzahh/events/MessageListener.java
@@ -1,43 +1,11 @@
 package io.github.pitzzahh.events;
 
-import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import org.jetbrains.annotations.NotNull;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class MessageListener extends ListenerAdapter {
-
-    /**
-     * Removed verfied role to a user when reaction is removed.
-     * @param event
-     */
-    @Override
-    public void onMessageReactionRemove(@NotNull MessageReactionRemoveEvent event) {
-        if (event.getEmoji().getName().equals("✅")) {
-            var channel = event
-                    .getGuild()
-                    .getTextChannelById("1008657297226088460");
-            var message = channel
-                    .getLatestMessageId();
-            if (message.equals("1009013499294396496")) {
-                var member = event.getMember();
-
-                var role = event
-                        .getGuild()
-                        .getRolesByName("verified", false)
-                        .stream()
-                        .findAny()
-                        .get();
-
-                event.getGuild()
-                        .removeRoleFromMember(member, role)
-                        .queue();
-            }
-        }
-    }
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
