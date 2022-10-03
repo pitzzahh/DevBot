1b2b6272dae8333ca52d0fb347db73dd2e6a151d
pitzzahh <araopj2191@gmail.com>
2022-10-03 22:30:50 +0800

    add Channels util for managing guild channels

---
+++ src/main/java/io/github/pitzzahh/utilities/Channels.java
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 pitzzahh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.pitzzahh.utilities;
+
+import io.github.pitzzahh.exceptions.ChannelNotFoundException;
+import net.dv8tion.jda.api.entities.PrivateChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
+import io.github.pitzzahh.Bot;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class Channels {
+
+    protected static List<TextChannel> TEXT_CHANNELS = Bot.getShardManager.get().getTextChannels();
+
+    protected static List<PrivateChannel> PRIVATE_CHANNELS = Bot.getShardManager.get().getPrivateChannels();
+
+    protected static List<VoiceChannel> VOICE_CHANNELS = Bot.getShardManager.get().getVoiceChannels();
+
+    public static TextChannel getTextChannel(String name) throws ChannelNotFoundException {
+        return TEXT_CHANNELS.stream()
+                .filter(e -> e.getName().equals(name))
+                .findAny()
+                .orElseThrow(() -> new ChannelNotFoundException(format("Cannot find: %s channel", name)));
+    }
+}
