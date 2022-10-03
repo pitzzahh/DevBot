a19d194ce67fc2359f3b102d9f694b58735c4e01
pitzzahh <araopj2191@gmail.com>
2022-10-03 22:29:35 +0800

    refactored [separated logic from lambda, done some changes]

--- src/main/java/io/github/pitzzahh/commands/slash_command/commands/Secret.java
+++ src/main/java/io/github/pitzzahh/commands/slash_command/commands/Secret.java
@@ -24,79 +24,113 @@
 
 package io.github.pitzzahh.commands.slash_command.commands;
 
+import static io.github.pitzzahh.utilities.Channels.getTextChannel;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import static java.time.format.DateTimeFormatter.ofLocalizedTime;
 import io.github.pitzzahh.commands.slash_command.CommandContext;
+import static io.github.pitzzahh.utilities.Util.MESSAGE_BUILDER;
 import io.github.pitzzahh.commands.slash_command.SlashCommand;
+import static io.github.pitzzahh.utilities.Util.EMBED_BUILDER;
 import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.MessageBuilder;
-import net.dv8tion.jda.api.EmbedBuilder;
-
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.concurrent.TimeUnit;
+import static io.github.pitzzahh.utilities.IChannels.*;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.time.format.FormatStyle.SHORT;
+import net.dv8tion.jda.api.entities.Message;
+import static java.time.LocalDateTime.now;
+import static java.lang.String.format;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.time.LocalDateTime;
+import static java.awt.Color.GREEN;
+import static java.time.ZoneId.of;
 import io.github.pitzzahh.Bot;
 import java.util.Objects;
-import java.time.ZoneId;
 import java.awt.*;
 
+/**
+ * Class used to manage secret slash command.
+ */
 public class Secret implements SlashCommand {
-
-    private final MessageBuilder MESSAGE_BUILDER = new MessageBuilder();
-    private final EmbedBuilder EMBED_BUILDER = new EmbedBuilder();
 
     @Override
     public Consumer<CommandContext> execute() {
-        return context -> {
-            if (context.getEvent().getChannel().getName().equals(Bot.getConfig.get().get("SECRET_CHANNEL"))) {
+        return this::process;
+    }
 
-                final var CONFESSIONS = Bot.getShardManager.get()
-                        .getTextChannelsByName(Bot.getConfig.get().get("SECRETS_CHANNEL"), false)
-                        .stream()
-                        .findAny();
+    /**
+     * Contains the process to be executed.
+     * @param context the command context containing the information about the command.
+     */
+    private void process(CommandContext context) {
+        if (context.getEvent().getChannel().getName().equals(ENTER_SECRET_CHANNEL)) {
 
-                final var message = Objects.requireNonNull(context.getEvent().getOption("secret-message")).getAsString();
+            final var CONFESSIONS = Bot.getShardManager.get()
+                    .getTextChannelsByName(SENT_SECRET_CHANNEL, false)
+                    .stream()
+                    .findAny();
 
-                EMBED_BUILDER.clear()
-                        .clearFields()
-                        .setColor(Color.RED)
-                        .setDescription(message)
-                        .setFooter("anonymous 👀")
-                        .setTimestamp(LocalDateTime.now(ZoneId.of("UTC")));
-                CONFESSIONS.ifPresent(
-                        c -> c.sendMessageEmbeds(EMBED_BUILDER.build())
-                                .queue(e -> {
-                                    EMBED_BUILDER.clear()
-                                            .setColor(Color.GREEN)
-                                            .setTitle("Secret message sent")
-                                            .setDescription("This message will be deleted")
-                                            .setFooter(
-                                                    String.format(
-                                                            "This message will be deleted on %s",
-                                                            LocalDateTime.now(ZoneId.of("UTC"))
-                                                                    .plusMinutes(1)
-                                                                    .format(
-                                                                            DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
-                                                                    )
-                                                    )
-                                            );
+            final var SECRET_MESSAGE = Objects.requireNonNull(context.getEvent().getOption("secret-message")).getAsString();
 
-                                    MESSAGE_BUILDER.clear()
-                                            .setEmbeds(EMBED_BUILDER.build());
-                                    context.getEvent()
-                                            .getInteraction()
-                                            .reply(MESSAGE_BUILDER.build())
-                                            .setEphemeral(true)
-                                            .queue();
-                                    e.delete().queueAfter(1, TimeUnit.MINUTES);
-                                })
+            EMBED_BUILDER
+                    .clear()
+                    .clearFields()
+                    .setColor(Color.RED)
+                    .setDescription(SECRET_MESSAGE)
+                    .setFooter("anonymous 👀")
+                    .setTimestamp(now(of("UTC")));
+            CONFESSIONS.ifPresent(
+                    c -> c.sendMessageEmbeds(EMBED_BUILDER.build())
+                            .queue(message -> confirmationMessage(context, message))
+            );
+        }
+        else {
+            EMBED_BUILDER.clear()
+                    .setColor(GREEN)
+                    .setTitle("Cannot use command here")
+                    .setDescription(format("To tell a secret message, go to %s", getTextChannel(ENTER_SECRET_CHANNEL).getAsMention()))
+                    .setFooter(format("This message will be deleted on %s",
+                                    now(of("UTC"))
+                                            .plusMinutes(1)
+                                            .format(ofLocalizedTime(SHORT))
+                            )
+                    );
+
+            MESSAGE_BUILDER
+                    .clear()
+                    .setEmbeds(EMBED_BUILDER.build());
+            context.getEvent()
+                    .getInteraction()
+                    .reply(MESSAGE_BUILDER.build())
+                    .setEphemeral(true)
+                    .queue();
+        }
+    }
+    private void confirmationMessage(CommandContext context, Message message) {
+        EMBED_BUILDER
+                .clear()
+                .setColor(GREEN)
+                .setTitle("Secret message sent")
+                .setDescription("This message will be deleted")
+                .setFooter(
+                        format("This message will be deleted on %s",
+                                now(of("UTC"))
+                                        .plusMinutes(1)
+                                        .format(
+                                                ofLocalizedTime(SHORT)
+                                        )
+                        )
                 );
-            }
-        };
+
+        MESSAGE_BUILDER
+                .clear()
+                .setEmbeds(EMBED_BUILDER.build());
+        context.getEvent()
+                .getInteraction()
+                .reply(MESSAGE_BUILDER.build())
+                .setEphemeral(true)
+                .queue();
+        message.delete().queueAfter(1, MINUTES);
     }
 
     @Override
