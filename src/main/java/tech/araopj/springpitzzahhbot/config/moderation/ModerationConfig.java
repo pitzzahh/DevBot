f4e81673364dc83440a528ffb0b80b38b573e37c
pitzzahh <pitzzahh@gmail.com>
2023-02-25 19:49:48 +0800

    refactor: moved config field values to MessageUtil

--- src/main/java/tech/araopj/springpitzzahhbot/config/moderation/ModerationConfig.java
+++ src/main/java/tech/araopj/springpitzzahhbot/config/moderation/ModerationConfig.java
@@ -1,22 +1,11 @@
 package tech.araopj.springpitzzahhbot.config.moderation;
 
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Bean;
 import java.util.*;
 
-@Getter
 @Configuration
 public class ModerationConfig {
-
-    @Value("${bot.moderation.reply-deletion-delay-in-minutes}")
-    public int replyDeletionDelayInMinutes;
-
-    @Value("${bot.moderation.message-deletion-delay-in-seconds}")
-    public int messageDeletionDelayInSeconds;
-
     @Bean
     public Map<String, Integer> violations() {
         return new HashMap<>();
