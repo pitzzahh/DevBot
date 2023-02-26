d5156083c8ff7309fea15de0b2c28df2278d0ee6
pitzzahh <pitzzahh@gmail.com>
2023-02-26 16:46:06 +0800

    remove: welcome category

--- src/main/java/tech/araopj/springpitzzahhbot/config/category/service/CategoryService.java
+++ src/main/java/tech/araopj/springpitzzahhbot/config/category/service/CategoryService.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public record CategoryService(CategoryConfig categoryConfig) {
 
-    public String welcomeCategoryName() {
-        return categoryConfig.getWelcomeCategory();
-    }
-
     public String updatesCategoryName() {
         return categoryConfig.getMemberUpdatesCategory();
     }
